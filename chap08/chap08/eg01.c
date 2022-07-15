2d9c8945176c30854a02c5124c659a25c79fdf07
zlh0024 <2567134812@qq.com>
2022-07-15 22:16:01 +0800

    提交代码

--- chap08/chap08/eg01.c
+++ chap08/chap08/eg01.c
@@ -154,37 +154,85 @@
 //}
 
 //例8.10：用指针方法对10个整数按由大到小顺序排序
+//#include<stdio.h>
+//int main()
+//{
+//	void sort(int a[],int n);
+//	int i,a[10]={0},* p;
+//	p=a;
+//	printf("输入数组a的元素：\n");
+//	for(i=0;i<10;i++,p++)
+//		scanf("%d",p);
+//	printf("\n");
+//	p=a;
+//	sort(p,10);
+//	printf("输出数组b的元素：\n");
+//	for(p=a;p<a+10;p++)
+//		printf("%d ",* p);
+//	printf("\n");
+//	return 0;
+//}
+//void sort(int a[],int n)
+//{
+//	int i,j,tmp;
+//	for(i=0;i<n-1;i++)
+//	{
+//		for(j=0;j<n-i-1;j++)
+//		{
+//			if(a[j]<a[j+1])
+//			{
+//				tmp=a[j];
+//				a[j]=a[j+1];
+//				a[j+1]=tmp;
+//			}
+//		}
+//	}
+//}
+
+//例8.11：输出二维数组的有关数据（地址和元素的值）
+//#include<stdio.h>
+//int main()
+//{
+//	int a[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
+//	printf("%d,%d\n",a,*a);
+//	printf("%d,%d\n",a[0],*(a+0));
+//	printf("%d,%d\n",&a[0],&a[0][0]);
+//	printf("%d,%d\n",a[1],a+1);
+//	printf("%d,%d\n",&a[1][0],*(a+1)+0);
+//	printf("%d,%d\n",a[2],*(a+2));
+//	printf("%d,%d\n",&a[2],a+2);
+//	printf("%d,%d\n",a[1][0],*(*(a+1)+0));
+//	printf("%d,%d\n",*a[2],*(*(a+2)+0));
+//	return 0;
+//}
+
+//指向数组元素的指针变量
+//例8.12： 有一个3*4的二维数组，要求用指向元素的指针变量输出二维数组名个元素的值
+//#include<stdio.h>
+//int main()
+//{
+//	int a[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
+//	int *p;
+//	for(p=a[0];p<a[0]+12;p++)
+//	{
+//		if((p-a[0])%4==0)
+//			printf("\n");
+//		printf("%4d",*p);
+//	}
+//	printf("\n");
+//	return 0;
+//}
+
+//例8.13：输出二维数组任一行任一列元素的值
 #include<stdio.h>
 int main()
 {
-	void sort(int a[],int n);
-	int i,a[10]={0},* p;
+	int a[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
+	int (*p)[4],i,j;
 	p=a;
-	printf("输入数组a的元素：\n");
-	for(i=0;i<10;i++,p++)
-		scanf("%d",p);
-	printf("\n");
-	p=a;
-	sort(p,10);
-	printf("输出数组b的元素：\n");
-	for(p=a;p<a+10;p++)
-		printf("%d ",* p);
+	printf("输入行和列分别为：");
+	scanf("%d%d",&i,&j);
+	printf("a[%d,%d]=%d",i,j,*(*(p+i)+j));
 	printf("\n");
 	return 0;
-}
-void sort(int a[],int n)
-{
-	int i,j,tmp;
-	for(i=0;i<n-1;i++)
-	{
-		for(j=0;j<n-i-1;j++)
-		{
-			if(a[j]<a[j+1])
-			{
-				tmp=a[j];
-				a[j]=a[j+1];
-				a[j+1]=tmp;
-			}
-		}
-	}
 }
